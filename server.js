85edc6bb8536de5996a201852416088253ab21cd
Nic <21686317@student.latrobe.edu.au>
2024-08-08 13:17:07 +1000

    Add fortune message

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 // Require the Express module 
 const express = require('express'); 
+// Create const for event handler
+const child_process = require('child_process');
+
 // Create an Express web application 
 const app = express(); 
+
 // Specify how to respond to GET / 
 app.get('/', (req, res) => { 
-    res.send('Hello World!'); 
+    //Run the system 'fortune' command and respond with the msg
+    child_process.exec('fortune',(error, message) => {
+        if(error == null) {
+            res.send(message);
+        } else {
+            res.send('Error:' + error);
+        }
+
+    })
+    //res.send('Hello World!'); 
 }); 
 // Start listening for HTTP requests on port 3000 
 app.listen(3000, () => { 
